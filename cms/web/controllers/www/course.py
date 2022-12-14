4e39f605bf6f2755c9c65361e5a52649142b242d
Jiang Alan <ericjiang8@163.com>
2022-12-14 11:36:37 +0800

    course搜索功能

--- cms/web/controllers/www/course.py
+++ cms/web/controllers/www/course.py
@@ -1,11 +1,38 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint,render_template,make_response,redirect
+from flask import Blueprint,render_template,request,make_response,redirect
+from common.models.gym.Course import Course
+from common.components.helper.DateHelper import DateHelper
+from common.components.helper.UtilHelper import UtilHelper
+from common.services.CommonConstant import CommonConstant
+
 
 route_www_course = Blueprint('www_course_page', __name__)
 
 @route_www_course.route("/")
 def index():
-    return render_template( "www/course/index.html")
+    req = request.values
+    page = int(req.get("p", 1))
+
+    category_ids = req['category_ids'].split(",") if ('category_ids' in req and req['category_ids']) else []
+
+    query = Course.query.filter_by(Course.status == 1)
+    if len(category_ids) > 0:
+        query.filter_by(Course.category_id.in_(category_ids))
+
+
+    page_params = {
+        "total": query.count(),
+        "page_size": CommonConstant.PAGE_SIZE,
+        "page": page,
+        "display": CommonConstant.PAGE_DISPLAY,
+    }
+
+    pages = UtilHelper.iPagination(page_params)
+    offset = (page - 1) * CommonConstant.PAGE_SIZE
+    limit = CommonConstant.PAGE_SIZE * page
+    list = query.order_by(Course.id.desc())[offset:limit]
+
+    return render_template( "www/course/index.html",{"list": list,"pages":pages })
 
 
 
